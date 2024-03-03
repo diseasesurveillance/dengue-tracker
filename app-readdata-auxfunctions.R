e2c37e12cb34df83ccee2e95dc7d0b061030487f
guipsoares <guilherme_soares@usp.br>
2024-03-03 14:49:27 +0300

    changing the website again

--- app-readdata-auxfunctions.R
+++ app-readdata-auxfunctions.R
@@ -55,8 +55,8 @@ library(viridis)
 library(shinyWidgets) # library("shinyWidgets")
 library(quantreg)
 library(lubridate)
-#library(INLA)
-#library(spdep)
+# library(INLA)
+# library(spdep)
 
 
 ############################################################################################
@@ -70,73 +70,71 @@ brazil_ufs <- c(
 )
 
 
-fnSetTimeLocVariablesDataMap <- function(d, map, dmapvbles){
-  
+fnSetTimeLocVariablesDataMap <- function(d, map, dmapvbles) {
   # time
   d$idtime <- d[, dmapvbles[1]]
   d$nametime <- d[, dmapvbles[2]]
-  
+
   # location 1
   d$idloc <- d[, dmapvbles[3]]
   # cuando hago join map y data pone nameloc.x nameloc.y. Asi que no pongo columna con name en data, solo en map
   # d$nameloc <- d$geocode
-  
+
   map$idloc <- map[, dmapvbles[4], drop = TRUE]
   map$nameloc <- map[, dmapvbles[5], drop = TRUE]
-  
+
   # location 2
   map$idloc2 <- map[, dmapvbles[6], drop = TRUE]
   map$nameloc2 <- map[, dmapvbles[7], drop = TRUE]
-  
+
   # To avoid the following error when doing left_join, remove attribute map
   # Warning: Error in stop_native_implementation: `vec_ptype2.character.character()` is implemented at C level
   attributes(map$idloc) <- NULL
   attributes(map$idloc2) <- NULL
-  
-  
+
+
   return(list(d, map))
-  
 }
 
 
 download_infodengue_data <- function(brazil_ufs) {
   last_ew_start <- Sys.Date() - wday(Sys.Date()) + 1
-  
+
   for (uf in brazil_ufs) {
-    
     infodengue_data <- denguetracker::fetch_data_from_state(uf,
-                                                            ey_start=2018,
-                                                            ey_end=2024)
+      ey_start = 2018,
+      ey_end = 2024
+    )
     filename <- sprintf("%s_%s_infodengue.csv", uf, last_ew_start)
-    file_path <- paste0("data/weekly_data/infodengue/",filename)
+    file_path <- paste0("data/weekly_data/infodengue/", filename)
     write.csv(infodengue_data, file_path, row.names = F)
     cat("\nSuccessfully saved ", filename, "\n")
-    
   }
 }
 
 process_data <- function(uf, last_ew_start) {
+  last_ew_start <- '2024-02-25'
   gt_filename <- sprintf("data/weekly_data/gtrends/%s_trends.csv", uf)
   cases_filename <- sprintf("data/weekly_data/infodengue/%s_%s_infodengue.csv", uf, last_ew_start)
-  
+
   cases <- read.csv(cases_filename, stringsAsFactors = FALSE)
   trends <- read.csv(gt_filename, stringsAsFactors = FALSE, skip = 2)
-  
+
   colnames(trends) <- gsub("\\.{3}.*$", "", colnames(trends))
   colnames(trends)[colnames(trends) == "Semana"] <- "Week"
-  
+
   min_week <- min(trends$Week)
-  
+
   cases <- cases |>
     filter(ew_start >= min_week)
-  
+
   cases$ew_start <- as.Date(cases$ew_start)
   trends$Week <- as.Date(trends$Week)
-  
+
   topics <- colnames(trends)[-1]
-  
+
   merged_data <- merge(cases, trends, by.x = "ew_start", by.y = "Week")
-  
+
   # Convert all "<1" values to 0 in all columns
   for (col in names(merged_data)) {
     # Skip the ew_start column
@@ -145,84 +143,89 @@ process_data <- function(uf, last_ew_start) {
       merged_data[[col]] <- ifelse(merged_data[[col]] == "<1", 0, as.numeric(merged_data[[col]]))
     }
   }
-  
+
   merged_data$uf <- uf
   return(list(as_tibble(merged_data), topics))
 }
 
 
-run_model <- function(merged_data, topics, log, K=5, gamma=0.95) {
-  if(log) {
+run_model <- function(merged_data, topics, log, K = 5, gamma = 0.95) {
+  if (log) {
     merged_data_log <- merged_data
-    merged_data_log$sum_of_cases <- log(merged_data$sum_of_cases+1)
-    merged_data_log[topics] <- log(merged_data[topics]+1)
-    
+    merged_data_log$sum_of_cases <- log(merged_data$sum_of_cases + 1)
+    merged_data_log[topics] <- log(merged_data[topics] + 1)
+
     formula_str <- paste("sum_of_cases ~ ", paste(topics, collapse = " + "))
-    best_linear_transform <- lm(as.formula(formula_str),
-                                merged_data_log[1:(nrow(merged_data_log)-5),])
-    prediction <- predict(best_linear_transform,merged_data_log)
-    error <- abs(prediction[1:(nrow(merged_data)-5)]-merged_data_log$sum_of_cases[1:(nrow(merged_data)-5)])
-    quantile_error <- quantile(error,probs=0.95)
-    merged_data_log$lwr=prediction-quantile_error
-    merged_data_log$upr=prediction+quantile_error
-    merged_data_log$lwr=exp(merged_data_log$lwr)-1
-    merged_data_log$upr=exp(merged_data_log$upr)-1
-    merged_data_log$prediction=exp(prediction)-1
-    merged_data_log$sum_of_cases=exp(merged_data_log$sum_of_cases)-1
+    best_linear_transform <- lm(
+      as.formula(formula_str),
+      merged_data_log[1:(nrow(merged_data_log) - 5), ]
+    )
+    prediction <- predict(best_linear_transform, merged_data_log)
+    error <- abs(prediction[1:(nrow(merged_data) - 5)] - merged_data_log$sum_of_cases[1:(nrow(merged_data) - 5)])
+    quantile_error <- quantile(error, probs = 0.95)
+    merged_data_log$lwr <- prediction - quantile_error
+    merged_data_log$upr <- prediction + quantile_error
+    merged_data_log$lwr <- exp(merged_data_log$lwr) - 1
+    merged_data_log$upr <- exp(merged_data_log$upr) - 1
+    merged_data_log$prediction <- exp(prediction) - 1
+    merged_data_log$sum_of_cases <- exp(merged_data_log$sum_of_cases) - 1
     return(merged_data_log)
   }
   formula_str <- paste("sum_of_cases ~ ", paste(topics, collapse = " + "))
-  best_linear_transform <- lm(as.formula(formula_str),
-                              merged_data[1:(nrow(merged_data)-K),])
-  prediction <- predict(best_linear_transform,merged_data)
-  
+  best_linear_transform <- lm(
+    as.formula(formula_str),
+    merged_data[1:(nrow(merged_data) - K), ]
+  )
+  prediction <- predict(best_linear_transform, merged_data)
+
   best_linear_transform_lower <- rq(as.formula(formula_str),
-                                    merged_data[1:(nrow(merged_data)-K),],
-                                    tau=(1-gamma)/2)
-  
-  prediction_lower <- predict(best_linear_transform_lower,merged_data)
-  
-  
+    merged_data[1:(nrow(merged_data) - K), ],
+    tau = (1 - gamma) / 2
+  )
+
+  prediction_lower <- predict(best_linear_transform_lower, merged_data)
+
+
   best_linear_transform_upper <- rq(as.formula(formula_str),
-                                    merged_data[1:(nrow(merged_data)-K),],
-                                    tau=1-(1-gamma)/2)
-  prediction_upper <- predict(best_linear_transform_upper,merged_data)
-  
-  error <- 
-    apply(cbind(prediction_lower[1:(nrow(merged_data)-K)]-merged_data$sum_of_cases[1:(nrow(merged_data)-K)]
-                ,merged_data$sum_of_cases[1:(nrow(merged_data)-K)]-prediction_upper[1:(nrow(merged_data)-K)]),1,max)
-  
-  quantile_error <- quantile(error,probs=0.95,na.rm=T)
-  merged_data$lwr=prediction_lower-quantile_error
-  merged_data$upr=prediction_upper+quantile_error
-  merged_data$prediction=prediction 
-  
+    merged_data[1:(nrow(merged_data) - K), ],
+    tau = 1 - (1 - gamma) / 2
+  )
+  prediction_upper <- predict(best_linear_transform_upper, merged_data)
+
+  error <-
+    apply(cbind(
+      prediction_lower[1:(nrow(merged_data) - K)] - merged_data$sum_of_cases[1:(nrow(merged_data) - K)],
+      merged_data$sum_of_cases[1:(nrow(merged_data) - K)] - prediction_upper[1:(nrow(merged_data) - K)]
+    ), 1, max)
+
+  quantile_error <- quantile(error, probs = 0.95, na.rm = T)
+  merged_data$lwr <- prediction_lower - quantile_error
+  merged_data$upr <- prediction_upper + quantile_error
+  merged_data$prediction <- prediction
+
   return(merged_data)
 }
 
 
-generate_data <- function(brazil_ufs, log=F) {
+generate_data <- function(brazil_ufs, log = F) {
   final_df <- data.frame()
   last_ew_start <- Sys.Date() - wday(Sys.Date()) + 1
-  
+
   for (uf in brazil_ufs) {
-    
     #### Data process
     out <- process_data(uf, last_ew_start)
     data <- out[[1]]
     topics <- out[[2]]
-    
+
     merged_data <- run_model(data, topics, log)
-    
+
     final_df <- rbind(final_df, merged_data)
     ##### Plots - Linear scale
-    #fig <- plot_data(merged_data, uf)
+    # fig <- plot_data(merged_data, uf)
     ##### Plots - Log scale
-    #fig_log <- plot_data(merged_data, uf, log=T)
-    #write.csv(final_df, "data/final_df.csv", row.names = F)
-    
+    # fig_log <- plot_data(merged_data, uf, log=T)
+    # write.csv(final_df, "data/final_df.csv", row.names = F)
   }
-  
+
   final_df
 }
-
--- tracker-createdatamaptimeplot.R
+++ tracker-createdatamaptimeplot.R
@@ -1,9 +1,8 @@
-
-fnMapFiltered <- function(vSelectTime, inputSelectLoc2, inputSelectVble){
+fnMapFiltered <- function(vSelectTime, inputSelectLoc2, inputSelectVble) {
   dtime <- d[which(d$idtime == vSelectTime), ]
   map <- left_join(map, dtime, by = c("idloc" = "idloc"))
   map <- map[which(map$idloc2 == inputSelectLoc2), ]
-  
+
   map$vble <- map[, inputSelectVble, drop = TRUE]
   return(map)
 }
@@ -11,88 +10,116 @@ fnMapFiltered <- function(vSelectTime, inputSelectLoc2, inputSelectVble){
 
 
 
-fnLeafletMap <- function(mapF){
+fnLeafletMap <- function(mapF) {
   labelOptionss <- labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "15px", direction = "auto")
-  
+
   # mapF <- mapFiltered()
   pal <- colorNumeric("YlOrRd", domain = mapF$vble)
   labels <- sprintf("<strong> %s </strong> <br/> %g ", mapF$nameloc, mapF$vble) %>%
     lapply(htmltools::HTML)
-  
-  leaflet(mapF) %>% addTiles() %>% #setView(lng = latIni, lat = longIni, zoom = zoomIni) %>%
-    addPolygons(fillColor = ~pal(vble), layerId = ~ idloc, group = "poly",
-                weight = 1, opacity = 1, color = "white", dashArray = "1", fillOpacity = 0.7,
-                label = labels, labelOptions = labelOptionss,
-                highlightOptions = highlightOptions(weight = 3, color = "gray", dashArray = "", fillOpacity = 0.7, bringToFront = FALSE)) %>%
+
+  leaflet(mapF) %>%
+    addTiles() %>% # setView(lng = latIni, lat = longIni, zoom = zoomIni) %>%
+    addPolygons(
+      fillColor = ~ pal(vble), layerId = ~idloc, group = "poly",
+      weight = 1, opacity = 1, color = "white", dashArray = "1", fillOpacity = 0.7,
+      label = labels, labelOptions = labelOptionss,
+      highlightOptions = highlightOptions(weight = 3, color = "gray", dashArray = "", fillOpacity = 0.7, bringToFront = FALSE)
+    ) %>%
     addLegend(pal = pal, values = ~vble, opacity = 0.7, title = NULL, position = "bottomright") %>%
-    addSearchFeatures(targetGroups  = "poly", options = searchFeaturesOptions(zoom = 7, autoCollapse = TRUE, openPopup = TRUE))
-  
-}  
+    addSearchFeatures(targetGroups = "poly", options = searchFeaturesOptions(zoom = 7, autoCollapse = TRUE, openPopup = TRUE))
+}
 
 
-fnDFiltered <- function(inputSelectVble, mapF){
-  dFiltered <-  as.data.frame(d)
+fnDFiltered <- function(inputSelectVble, mapF) {
+  dFiltered <- as.data.frame(d)
   dFiltered <- dFiltered[which(dFiltered$idloc %in% mapF$idloc), ] # select idloc in mapF (in state)
   dFiltered$vble <- dFiltered[, inputSelectVble]
-  return(dFiltered)  
+  return(dFiltered)
 }
 
-fnTimeplot <- function(dF){
+fnTimeplot <- function(dF) {
   dF <- dF[order(dF$idtime), ]
-  apex(dF, aes(x = idtime, y = vble, group = idloc), type = "line")# %>%
-  # ax_colors(c("#8485854D", "#FF0000")) %>% ax_stroke(width = c(3, 2))%>% 
+  apex(dF, aes(x = idtime, y = vble, group = idloc), type = "line") # %>%
+  # ax_colors(c("#8485854D", "#FF0000")) %>% ax_stroke(width = c(3, 2))%>%
   #  ax_fill(opacity = 1, type = "solid")
   # %>% add_event_marker(as.Date(input$selectTime), y = 0)
-  
 }
 
-plot_trends_data <- function(merged_data, uf, K=5) {
-  date_no_delay <- merged_data[nrow(merged_data)-K,]$ew_start
-  
+plot_trends_data <- function(merged_data, uf, K = 5) {
+  date_no_delay <- merged_data[nrow(merged_data) - K, ]$ew_start
+  merged_data <- merged_data %>% filter(ew_start >= '2021-01-01')
   fig <- ggplot(merged_data) +
-    geom_line(aes(x=ew_start,y = sum_of_cases, group = 1, 
-                  colour = "Reported Cases \n (subject to delays)"),
-              size=1) +
-    geom_line(data=merged_data %>% filter(ew_start<=date_no_delay),aes(x = ew_start,y = prediction, 
-                                                                       group = 1, color = "Fitted Model"),linetype=1, size = .5) +
-    geom_ribbon(data=merged_data %>% filter(ew_start<=date_no_delay),aes(x = ew_start,ymin=lwr, ymax=upr),
-                linetype=2, size = .5,alpha=0.1,fill = "#004D40",color="#1E88E5")+
-    geom_line(data=merged_data %>% filter(ew_start>=date_no_delay),aes(x = ew_start,y = prediction, 
-                                                                       group = 1, color = "Corrected Estimate"),size=1) +
-    geom_ribbon(data=merged_data %>% filter(ew_start>=date_no_delay),aes(x = ew_start,ymin=lwr, ymax=upr),
-                fill = "#D81B60", linetype=2, alpha=0.3)+
+    geom_line(
+      aes(
+        x = ew_start, y = sum_of_cases, group = 1,
+        colour = "Reported Cases \n (subject to delays)"
+      ),
+      size = 1
+    ) +
+    geom_line(data = merged_data %>% filter(ew_start <= date_no_delay), aes(
+      x = ew_start, y = prediction,
+      group = 1, color = "Fitted Model"
+    ), linetype = 1, size = .5) +
+    geom_ribbon(
+      data = merged_data %>% filter(ew_start <= date_no_delay), aes(x = ew_start, ymin = lwr, ymax = upr),
+      linetype = 2, size = .5, alpha = 0.1, fill = "#004D40", color = "#1E88E5"
+    ) +
+    geom_line(data = merged_data %>% filter(ew_start >= date_no_delay), aes(
+      x = ew_start, y = prediction,
+      group = 1, color = "Corrected Estimate"
+    ), size = 1) +
+    geom_ribbon(
+      data = merged_data %>% filter(ew_start >= date_no_delay), aes(x = ew_start, ymin = lwr, ymax = upr),
+      fill = "#D81B60", linetype = 2, alpha = 0.3
+    ) +
     labs(x = "", y = "Total Number of Weekly Cases") +
-    theme(axis.text.x = element_text(size=18), legend.text = element_text(size = 14),
-          legend.title = element_text( size = 16,face="bold"),
-          axis.title=element_text(size=18),plot.title = element_text(size=12))+
-    theme_bw()+
-    #scale_x_date(break.vec, date_labels = "%m/%y")+
-    scale_x_date(date_breaks = "3 month",
-                 date_labels = "%b/%y")+
-    #scale_fill_manual(values = c("fitted" = "#004D40")) + 
-    scale_colour_manual("", 
-                        breaks = c("Reported Cases \n (subject to delays)", "Corrected Estimate","Fitted Model"),
-                        values = c("Reported Cases \n (subject to delays)" = "#004D40", 
-                                   "Corrected Estimate" = "#D81B60",
-                                   "Fitted Model"="#1E88E5")) +
-    theme(legend.position = c(.1, .9),
-          legend.key.size = unit(1.2,"line"),
-          legend.key.width= unit(2, 'line'),
-          legend.text=element_text(size=12),
-          axis.text.x = element_text(size = 12, angle = 45, colour = "black",
-                                     vjust = 1, hjust = 1), 
-          axis.title = element_text(size = 12), 
-          panel.grid.minor = element_blank(), 
-          axis.line = element_line(colour = "black"), 
-          panel.border = element_blank(), 
-          panel.grid.major.x = element_blank(), 
-          panel.grid.major.y = element_line(color = "gray79",
-                                            size = 0.25, 
-                                            linetype = 4))+
-    coord_cartesian(expand = FALSE) +  
-    scale_y_continuous(labels = scales::comma)+
-    ggtitle(paste0("Dengue in the state of São Paulo (Brazil)\nLast updated using reported cases until ",
-                   max(merged_data$ew_start)))
+    theme(
+      axis.text.x = element_text(size = 18), legend.text = element_text(size = 14),
+      legend.title = element_text(size = 16, face = "bold"),
+      axis.title = element_text(size = 18), plot.title = element_text(size = 12)
+    ) +
+    theme_bw() +
+    # scale_x_date(break.vec, date_labels = "%m/%y")+
+    scale_x_date(
+      date_breaks = "2 month",
+      date_labels = "%b/%y"
+    ) +
+    # scale_fill_manual(values = c("fitted" = "#004D40")) +
+    scale_colour_manual("",
+      breaks = c("Reported Cases \n (subject to delays)", "Corrected Estimate", "Fitted Model"),
+      values = c(
+        "Reported Cases \n (subject to delays)" = "#004D40",
+        "Corrected Estimate" = "#D81B60",
+        "Fitted Model" = "#1E88E5"
+      )
+    ) +
+    theme(
+      legend.position = c(.15, .9),
+      legend.key.size = unit(1.2, "line"),
+      legend.key.width = unit(2, "line"),
+      legend.text = element_text(size = 12),
+      axis.text.x = element_text(
+        size = 12, angle = 45, colour = "black",
+        vjust = 1, hjust = 1
+      ),
+      axis.title = element_text(size = 12),
+      panel.grid.minor = element_blank(),
+      axis.line = element_line(colour = "black"),
+      panel.border = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_line(
+        color = "gray79",
+        size = 0.25,
+        linetype = 4
+      )
+    ) +
+    coord_cartesian(expand = FALSE) +
+    scale_y_continuous(labels = scales::comma) +
+    ggtitle(paste0(
+      "Dengue in ", uf, " (Brazil)\nLast updated using reported cases until ",
+      max(merged_data$ew_start)
+    ))
   ggsave(sprintf("figures/%s_plot.png", uf), plot = fig, width = 11, height = 7)
   return(fig)
 }
@@ -104,4 +131,4 @@ render_files <- function(folder_root_directory) {
     setwd(folder_root_directory)
     rmarkdown::render(input = file, output_file = paste0("../docs/", filename, ".html"))
   }
-} 
+}
